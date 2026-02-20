41906ffcec2082e13ba3c8dd75dc7ee2c9acff16
Pritesh23-P <pritesh0525@gmail.com>
2026-02-21 04:50:50 +0530

    Prepared project for Vercel deployment

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import cors from "cors";
 import cardRoutes from "./routes/cardRoutes.js";
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Use Routes
+// Routes
 app.use("/cards", cardRoutes);
 
-// Home Route
 app.get("/", (req, res) => {
   res.json({ message: "Playing Card Collection API Running" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// 👇 This makes it work locally but not interfere with Vercel
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
